b9872054ea063affd7459971e0f5c4f83d19050c
pawan459 <pawan18092019@gmail.com>
2020-12-14 15:17:04 +0530

    text added in app.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          mohit rajput deployed first time!
+          change made in feature branch!
         </p>
         <a
           className="App-link"
